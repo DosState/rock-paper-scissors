d3890463dbc1232e30b08a6806f647093cdf1fd1
DosState <francescobleggi@gmail.com>
2025-07-06 19:54:38 +0200

    add random rock paper scissor function

--- rps.js
+++ rps.js
@@ -1,3 +1,18 @@
 function rps(){
+    let choice = Math.random()*3;
+    if (choice>=0 && choice<1){
+        return "rock"
+    }
+    else if (choice>=1 && choice<2){
+        return "paper"
+    }
+    else if (choice>=2 && choice<=3){
+        return "scissors"
+    }
+    else{
+        return "I don't know"
+    }
 }
+
 console.log("Let's play!")
+console.log(rps())
