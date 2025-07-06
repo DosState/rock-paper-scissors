a850794bb09ade58e65a8db6201dde0a8dca61e5
DosState <francescobleggi@gmail.com>
2025-07-06 20:00:29 +0200

    add human choice function to game

--- rps.js
+++ rps.js
@@ -14,5 +14,15 @@ function rps(){
     }
 }
 
+function getHumanChoice(){
+    let choice = prompt("rock paper or scissors")
+    if (choice !== "rock" || choice !=="paper" || choice!=="scissors"){
+        return undefined
+    }
+    return choice
+}
+
+
 console.log("Let's play!")
 console.log(rps())
+console.log(getHumanChoice())
